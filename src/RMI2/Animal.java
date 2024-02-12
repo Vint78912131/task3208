ed7cede355b7cf23b75e7783b98fd9fb5e8f962a
v sotnikov <sotnikov@rosplatforma.ru>
2024-02-12 11:28:38 +0300

    12022024commit

---
+++ src/RMI2/Animal.java
@@ -0,0 +1,10 @@
+package RMI2;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Animal extends Remote {
+    void speak() throws RemoteException;
+
+    void printName() throws RemoteException;
+}
